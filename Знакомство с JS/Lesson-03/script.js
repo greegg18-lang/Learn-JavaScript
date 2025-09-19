6f41ccffe522d6b330c3c785411b3cf1e90bde82
greegg18-lang <greegg18@gmail.com>
2025-09-19 19:04:32 +0300

    Add files via upload

---
+++ Знакомство с JS/Lesson-03/script.js
@@ -0,0 +1,7 @@
+
+let firstname = prompt('Назови своё имя', '');
+let middlename = prompt('Назови своё отчество', '');
+let lastname = prompt('Назови свою фамилию', '');
+console.log(firstname,middlename,lastname);
+alert('Hello '+ firstname + " " + middlename + " " +lastname);
+
